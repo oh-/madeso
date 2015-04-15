f07f91207480f9805438485a6d2aebb8abf9bdf2
Samuel Overington <so@madeso.uk>
2015-04-15 18:32:09 +0100

    moved some things around

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,35 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task
+// var del = require('del');
+// var fs = require('fs');
+
+
+
+
+gulp.task('sass', function() {
+	return sass('scss/')
+		.on('error', function (err) {
+			console.error('Error! (sass)', err.message);
+		})
+		.pipe(gulp.dest('wp/'))
+		.pipe(reload({ stream:true }))
+});
+
+//  browsersync config
+var config = {
+	files: ['wp/style.css', 'wp/*.php'],
+	proxy: "localhost/ecostage/?$",
+	notify: "false"
+};
+
+gulp.task('serve', function() {
+	browserSync(config);
+	gulp.watch('scss/**.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'serve'], function() {
+	gulp.watch("scss/**/*.scss", ['sass']);
+});
