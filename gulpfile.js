1b2f0acece2789089e364ded38b3cb93c1596bb5
Samuel Overington <so@madeso.uk>
2015-04-19 17:04:01 +0100

    gulp live reload works, with susy. Not the pretties of gulp files, so could use some prettying up, but nonetheless, works for monday morning startup. or now.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
@@ -9,18 +10,20 @@ var reload = browserSync.reload;
 
 var SassFiles = {
 	soc: 'app/theme/sass/style.scss',
-	dst: 'app/theme/'
+	dst: 'app/theme/',
+	sf: './app/theme/sass/{,*/}*.{scss,sass}'
 };
 
 // Gulp Sass Task 
 gulp.task('sass', function() {
   gulp.src('./app/theme/sass/{,*/}*.{scss,sass}')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
+   	.pipe(sourcemaps.init())
+   	.pipe(sass({
       errLogToConsole: true
     }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./app/theme'));
+    .pipe(gulp.dest('./app/theme'))
+    .pipe(reload({ stream:true }));
 })
 
 
@@ -50,5 +53,6 @@ gulp.task('serve', function() {
 });
 
 gulp.task('default', ['sass', 'serve'], function() {
-	gulp.watch('app/theme/**.scss', ['sass']);
+	// gulp.watch('./app/theme/sass/**.scss', ['sass']);
+	gulp.watch('./app/theme/sass/{,*/}*.{scss,sass}', ['sass']);
 });
