6161995d848d59344a03cdacb16cd83fd80d49c1
Samuel Overington <so@madeso.uk>
2015-04-22 09:38:58 +0100

    changed gulpfile slightly

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+// Site vars
+
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
@@ -5,8 +7,6 @@ var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-// var del = require('del');
-// var fs = require('fs');
 
 var SassFiles = {
 	soc: 'app/theme/sass/style.scss',
@@ -24,26 +24,14 @@ gulp.task('sass', function() {
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./app/theme'))
     .pipe(reload({ stream:true }));
-})
+});
 
 
-
-// gulp-ruby-sass task info for sass
-// gulp.task('sass', function() {
-// 	// Sass watch
-// 	// var watcher = gulp.watch('SassFiles(src)', [sass])
-// 	return sass('app/theme/sass/', {sass }
-// 		.on('error', function (err) {
-// 			console.error('Error! (sass)', err.message);
-// 		})
-// 		.pipe(gulp.dest('app/theme'))
-// 		.pipe(reload({ stream:true }))
-// });
 
 //  browsersync config
 var config = {
 	files: ['app/theme/style.css', 'app/theme/*.php'],
-	proxy: "localhost/madeso/",
+	proxy: "localhost/ecostage/",
 	notify: "false"
 };
 
