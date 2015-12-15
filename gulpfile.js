5ebe493c7009b8716fbc63fe7c31909860ec9dca
Samuel Overington (neon) <so@madeso.uk>
2015-12-15 13:23:32 +0000

    updated gulpfile to gulp new --name 'Full Name'

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,41 @@
+// buildworld
+
 var gulp         = require('gulp');
-var browserSync  = require('browser-sync');
-var filter       = require('gulp-filter');
+var util         = require('gulp-util');
+var replace      = require('gulp-replace');
 var sass         = require('gulp-ruby-sass');
+var filter       = require('gulp-filter');
 var sourcemaps   = require('gulp-sourcemaps');
+var browserSync  = require('browser-sync');
 var reload       = browserSync.reload;
 
 var theme = {
   url: 'base.dev',
   dir: 'app/theme',
-  bower: 'bower_components/'
+  tmp: 'app/tmp',
+  bower: 'bower_components/',
+  _s: 'bower_components/_s/'
 };
+
+
 var config = {
   src: {
     scss: theme.dir+'/**/*.scss',
     css:  theme.dir,
     php: theme.dir+'*.php'
   },
+  WordFrom: [
+    // Search for '_s' (inside single quotations) to capture the text domain.
+    "'_s'",
+    // Search for _s_ to capture all the function names.
+    '_s_',
+    // Search for Text Domain: _s in style.css.
+    'Text Domain: _s',
+    // Search for  _s (with a space before it) to capture DocBlocks.
+    ' _s',
+    // Search for _s- to capture prefixed handles.
+    '_s-'
+  ],
 
   sassloadpath: [
     theme.bower + 'compass-mixins/lib/',
@@ -78,4 +98,51 @@ gulp.task('clear', function() {
 /**
  * Default task
  */
-gulp.task('default', ['serve']);
+// gulp.task('default', ['serve']);
+
+var ThemeName = '';
+var textdomain = '';
+
+function GetThemeName () {
+  if (util.env.name == null ) {
+   return ThemeName = 'error',
+console.log('you must Specifiy a name using --name SingleWordName');
+  } else { ThemeName = util.env.name;
+  ltext = ThemeName.replace(/ /gi, "");
+  textdomain = ltext.toLowerCase();
+  return ThemeName, textdomain;
+} };
+
+// example 
+// var re = /(\w+)\s(\w+)/;
+// var str = 'John Smith';
+// var newstr = str.replace(re, '$2, $1');
+// console.log(newstr);  // Smith, John
+
+function Rename (tn, td) {
+  gulp.src([ theme._s + '**/*' ])
+  // .pipe(replace( new RegExp("'_s'", "'" + td + "'")))
+  
+  .pipe(replace(config.WordFrom[0], "'" + td + "'"))
+  .pipe(replace(config.WordFrom[1], td + "_"))
+  .pipe(replace(config.WordFrom[3], " " + tn ))
+  .pipe(replace(config.WordFrom[4], td + "-"))
+  // .pipe(replace( new RegExp("'_s'", "'" + td + "'")))
+  // .pipe(replace( new RegExp("_s_",  td + "_")))
+  // .pipe(replace( new RegExp(" _s", " " + tn )))
+  // .pipe(replace( new RegExp("_s-",  td + "-")))
+  .pipe(gulp.dest('app/' + td +'/'));
+}
+
+
+/*
+ * Create new theme based off the most current version of _s,
+ * and hopefully rename all the variables
+ */
+
+
+gulp.task('new', function (){
+  GetThemeName();
+  Rename(ThemeName, textdomain);
+  console.log(ThemeName), console.log(textdomain);
+});
